5ac6af0261637d365a6bb3931898dac77699026f
ronrole <ronrolenewadress@gmail.com>
2020-11-05 18:01:24 +0900

    mysqlとの連携テスト

---
+++ golang_mysql/src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func randomFromDB() float64 {
+	db, err := sql.Open("mysql", "root:root@tcp(mysql)/test")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	var value float64;
+
+	results, err := db.Query("SELECT RAND()")
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next() {
+		results.Scan(&value)
+	}
+	defer results.Close()
+
+	return value;
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w,strconv.FormatFloat(randomFromDB(), 'f', -1, 64))
+	})
+	http.ListenAndServe(":9090", nil)
+}
