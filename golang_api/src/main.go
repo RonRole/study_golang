a2499644428e5eef80a1b76c78a110fe07a35f27
ronrole <ronrolenewadress@gmail.com>
2020-11-12 12:14:03 +0900

    qiitaのapiをたたいてみる

--- golang_api/src/main.go
+++ golang_api/src/main.go
@@ -6,4 +6,34 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
 )
+
+type Item struct {
+	Title string `json:"title"`
+	Url string `json:"url"`
+}
+
+func main() {
+	response, err := http.Get("https://qiita.com/api/v2/items")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var items []Item
+	marshalErr := json.Unmarshal(responseData, &items)
+	if marshalErr != nil {
+		log.Fatal(err)
+	}
+
+	for _, item := range items {
+		fmt.Printf("title:%v, url:%v\n", item.Title, item.Url)
+	}
+}
